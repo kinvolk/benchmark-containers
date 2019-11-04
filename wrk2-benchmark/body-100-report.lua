39248d53580b8513f7522491293a11de8a5e864d
Kai Lüke <kai@kinvolk.io>
2019-11-04 12:29:03 +0100

    wrk2-benchmark: wrk2 container for HTTP req/s latency benchmarks
    Based on
    https://github.com/kinvolk/service-mesh-benchmark/tree/master/wrk2
    (can be found in https://quay.io/repository/kinvolk/wrk2)
    with modifications to include lscpu and remove unused parts.

---
+++ wrk2-benchmark/body-100-report.lua
@@ -0,0 +1,11 @@
+wrk.body = "____________________________________________________________________________________________________"
+function done(summary, latency, requests)
+    print(string.format("Total Requests: %d", summary.requests))
+    print(string.format("HTTP errors: %d", summary.errors.status))
+    print(string.format("Requests timed out: %d", summary.errors.timeout))
+    print(string.format("Bytes received: %d", summary.bytes))
+    print(string.format("Socket connect errors: %d", summary.errors.connect))
+    print(string.format("Socket read errors: %d", summary.errors.read))
+    print(string.format("Socket write errors: %d", summary.errors.write))
+end
+
