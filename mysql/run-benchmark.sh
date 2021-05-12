cdcf9104c02163913f34bfd217154627f9f9c426
Suraj Deshmukh <suraj@kinvolk.io>
2021-05-13 00:49:00 +0530

    mysql: Use MySQL over MariaDB
    - This commit adds support of MySQL 8 over MariaDB.
      - Use old authentication method `mysql_native_password` overriding the
        new one `caching_sha2_password`.
    
    - Container Image:
      - Build image on top of mysql:8.
      - Download pre-built sysbench.
      - Add Debian based Dockerfile for benchmark-base.
    
    - run-server.sh
      - Modify this script to only create the necessary directories.
      - Initialise MySQL server.
      - Start MySQL server.
    
    - run-benchmark.sh
      - Move the database creation part from `run-server.sh` to
        `run-benchmark.sh`, since it is not a part of the initialisation any
        more.
      - Add support for iterating over multiple thread values.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- mysql/run-benchmark.sh
+++ mysql/run-benchmark.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 . /usr/local/bin/output.sh
+cd /tmp
 
 usage() {
   cat <<EOF
@@ -45,28 +46,28 @@ parse_params() {
   done
 }
 
-parse_params "$@"
+start_database() {
+  echo "[i] Starting the database server..."
 
-SYSTEM=$(/usr/local/bin/cpu.sh system)
-PARAMS="--threads=$THREADS"
+  # Run the mysql server
+  /usr/local/bin/run-server.sh "${MYSQL_DATABASE}" &
 
-echo "Starting the database server..."
-# Run the mysql server
-/usr/local/bin/run-server.sh "${MYSQL_DATABASE}" &
+  trap 'killall mysqld || true' SIGHUP SIGTERM SIGINT EXIT
 
-trap 'killall mysqld || true' SIGHUP SIGTERM SIGINT EXIT
+  # Check connection to mysql
+  until mysql -u root --skip-password -e ";" ; do
+    sleep 5
+    echo "[i] Waiting for DB to start..."
+  done
 
-# Check connection to mysql
-until mysql -u root -e ";" ; do
-  sleep 1
-  echo "Waiting for DB to start..."
-done
+  # Create the database.
+  echo "[i] Creating database $MYSQL_DATABASE"
+  mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+}
 
-cd /tmp
-
-echo "Creating json conversion file."
-
-cat >output_json.lua <<EOF
+setup_sysbench_config() {
+  echo "[i] Creating JSON conversion file."
+  cat >output_json.lua <<EOF
 ffi.cdef[[int usleep(unsigned int);]]
 
 function event()
@@ -75,73 +76,84 @@ end
 
 sysbench.hooks.report_intermediate = sysbench.report_json
 EOF
+}
 
-BENCHMARK_NAME="mysql"
+benchmark() {
 
-echo "This is iterations $ITERATIONS"
-for I in $(seq 1 $ITERATIONS); do
-  ts=$(date -Iseconds | sed -e 's/T/__/' -e 's/+.*//')
-  echo "Preparing benchmark"
-  sysbench $PARAMS --db-driver=mysql --mysql-user=root --tables=16 --table-size=1000000 \
-    --rand-type=uniform --mysql-db="$MYSQL_DATABASE" /usr/local/share/sysbench/oltp_read_write.lua \
-    prepare
+  BENCHMARK_NAME="mysql"
+  PARAMS="--threads=$THREADS --db-driver=mysql --mysql-user=root --tables=16 --table-size=1000000 \
+    --rand-type=uniform --mysql-db=$MYSQL_DATABASE"
 
-  echo "Running benchmark"
-  sysbench $PARAMS --db-driver=mysql --mysql-user=root --tables=16 --table-size=1000000 \
-    --rand-type=uniform --mysql-db="$MYSQL_DATABASE" --time=100 --report-interval=1 --verbosity=5 \
-    --rate=40 --events=0 /tmp/output_json.lua run > /tmp/output-$I.txt
+  for I in $(seq 1 $ITERATIONS); do
+    ts=$(date -Iseconds | sed -e 's/T/__/' -e 's/+.*//')
+    echo "[i] Preparing benchmark"
+    sysbench $PARAMS /usr/share/sysbench/oltp_read_write.lua prepare
 
-  echo "Running cleanup"
-  sysbench $PARAMS --db-driver=mysql --mysql-user=root --tables=16 --table-size=1000000 \
-    --rand-type=uniform --mysql-db="$MYSQL_DATABASE" /usr/local/share/sysbench/oltp_read_write.lua \
-    cleanup
+    echo "[i] Running benchmark"
+    sysbench $PARAMS --time=100 --report-interval=1 --verbosity=5 --rate=40 --events=0 \
+      /tmp/output_json.lua run > /tmp/output-$I.txt
 
-  # General Statistics
-  total_time=$(cat /tmp/output-$I.txt | grep -A 3 "General statistics" | grep "total time:" |cut -d":" -f2| xargs | cut -d "s" -f1)
-  total_events=$(cat /tmp/output-$I.txt | grep -A 3 "General statistics" | grep "total number of events:" |cut -d":" -f2 |xargs)
+    echo "[i] Running cleanup"
+    sysbench $PARAMS /usr/share/sysbench/oltp_read_write.lua cleanup
 
-  # Latency
-  latency_min=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "min:" |cut -d":" -f2 |xargs)
-  latency_max=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "max:" |cut -d":" -f2 |xargs)
-  latency_avg=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "avg:" |cut -d":" -f2 |xargs)
-  latency_percentile=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "percentile:" |cut -d":" -f2 |xargs)
-  latency_sum=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "sum:" |cut -d":" -f2 |xargs)
+    # General Statistics
+    total_time=$(cat /tmp/output-$I.txt | grep -A 3 "General statistics" | grep "total time:" |cut -d":" -f2| xargs | cut -d "s" -f1)
+    total_events=$(cat /tmp/output-$I.txt | grep -A 3 "General statistics" | grep "total number of events:" |cut -d":" -f2 |xargs)
 
-  # Thread fairness
-  thread_fairness_events=$(cat /tmp/output-$I.txt | grep -A2 "Threads fairness" | grep "events" |cut -d":" -f2 |xargs | cut -d "/" -f1)
-  thread_fairness_execution_time=$(cat /tmp/output-$I.txt | grep -A2 "Threads fairness" | grep "execution time" |cut -d":" -f2 |xargs | cut -d "/" -f1)
+    # Latency
+    latency_min=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "min:" |cut -d":" -f2 |xargs)
+    latency_max=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "max:" |cut -d":" -f2 |xargs)
+    latency_avg=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "avg:" |cut -d":" -f2 |xargs)
+    latency_percentile=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "percentile:" |cut -d":" -f2 |xargs)
+    latency_sum=$(cat /tmp/output-$I.txt | grep -A5 "Latency (ms)" | grep "sum:" |cut -d":" -f2 |xargs)
 
-  # JSON data extracted from txt file and converted to json
-  sed -n '18,/Time limit exceeded, exiting.../p' /tmp/output-$I.txt | sed 's/Time limit exceeded, exiting...//' | sed '${s/$/]/}' > output-$I.json
-  sed -i 's|^DEBUG.*||g; s|^Threads started.*||g' /tmp/output-$I.json
+    # Thread fairness
+    thread_fairness_events=$(cat /tmp/output-$I.txt | grep -A2 "Threads fairness" | grep "events" |cut -d":" -f2 |xargs | cut -d "/" -f1)
+    thread_fairness_execution_time=$(cat /tmp/output-$I.txt | grep -A2 "Threads fairness" | grep "execution time" |cut -d":" -f2 |xargs | cut -d "/" -f1)
 
-  RUN_ID="$ts"
-  read -d '' metrics <<EOF || true
-    # TYPE mysql_general_statistics_total_ gauge
-    $(echo "mysql_general_statistics_total_time ${total_time}")
-    $(echo "mysql_general_statistics_total_events ${total_events}")
+    # JSON data extracted from txt file and converted to json
+    sed -n '18,/Time limit exceeded, exiting.../p' /tmp/output-$I.txt | sed 's/Time limit exceeded, exiting...//' | sed '${s/$/]/}' > output-$I.json
+    sed -i 's|^DEBUG.*||g; s|^Threads started.*||g' /tmp/output-$I.json
 
-    # TYPE mysql_latency_ gauge
-    $(echo "mysql_latency_min ${latency_min}")
-    $(echo "mysql_latency_max ${latency_max}")
-    $(echo "mysql_latency_avg ${latency_avg}")
-    $(echo "mysql_latency_percentile ${latency_percentile}")
-    $(echo "mysql_latency_sum ${latency_sum}")
+    RUN_ID="$ts/threads/$THREADS"
+    read -d '' metrics <<EOF || true
+      # TYPE mysql_general_statistics_total_ gauge
+      $(echo "mysql_general_statistics_total_time ${total_time}")
+      $(echo "mysql_general_statistics_total_events ${total_events}")
 
-    # TYPE mysql_thread_fairness_ gauge
-    $(echo "mysql_thread_fairness_events ${thread_fairness_events}")
-    $(echo "mysql_thread_fairness_execution_time ${thread_fairness_execution_time}")
+      # TYPE mysql_latency_ gauge
+      $(echo "mysql_latency_min ${latency_min}")
+      $(echo "mysql_latency_max ${latency_max}")
+      $(echo "mysql_latency_avg ${latency_avg}")
+      $(echo "mysql_latency_percentile ${latency_percentile}")
+      $(echo "mysql_latency_sum ${latency_sum}")
 
-    # TYPE mysql_benchmark_result_ gauge
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_threads{time="+ "\"" + (.time|tostring) + "\"} " + (.threads|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_tps{time="+ "\"" + (.time|tostring) + "\"} " + (.tps|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_latency{time="+ "\"" + (.time|tostring) + "\"} " + (.latency|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_errors{time="+ "\"" + (.time|tostring) + "\"} " + (.errors|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_reconnects{time="+ "\"" + (.time|tostring) + "\"} " + (.reconnects|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_total{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.total|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_reads{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.reads|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_writes{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.writes|tostring)')
-    $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_other{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.other|tostring)')
+      # TYPE mysql_thread_fairness_ gauge
+      $(echo "mysql_thread_fairness_events ${thread_fairness_events}")
+      $(echo "mysql_thread_fairness_execution_time ${thread_fairness_execution_time}")
+
+      # TYPE mysql_benchmark_result_ gauge
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_threads{time="+ "\"" + (.time|tostring) + "\"} " + (.threads|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_tps{time="+ "\"" + (.time|tostring) + "\"} " + (.tps|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_latency{time="+ "\"" + (.time|tostring) + "\"} " + (.latency|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_errors{time="+ "\"" + (.time|tostring) + "\"} " + (.errors|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_reconnects{time="+ "\"" + (.time|tostring) + "\"} " + (.reconnects|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_total{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.total|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_reads{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.reads|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_writes{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.writes|tostring)')
+      $(cat /tmp/output-1.json | jq -r '.[] | "mysql_benchmark_result_qps_other{time="+ "\"" + (.time|tostring) + "\"} " + (.qps.other|tostring)')
 EOF
-  push_metrics "$metrics"
+    push_metrics "$metrics"
+  done
+
+}
+
+parse_params "$@"
+start_database
+setup_sysbench_config
+
+
+for THREADS in ${ARG_THREADS//;/ }; do
+  echo "[i] Running for threads ${THREADS}."
+  benchmark
 done
--- mysql/run-server.sh
+++ mysql/run-server.sh
@@ -1,48 +1,14 @@
-if [ ! -d "/run/mysqld" ]; then
-  mkdir -p /run/mysqld
-fi
+#!/bin/bash
+
+mkdir -p /run/mysqld
+mkdir -p /var/lib/mysql-files
 
 if [ -d /app/mysql ]; then
-  echo "[i] MySQL directory already present, skipping creation"
+  echo "[i] MySQL directory already present, skipping initialization."
 else
-  echo "[i] MySQL data directory not found, creating initial DBs"
+  echo "[i] MySQL data directory not found, initializing DB."
 
-  mysql_install_db --user=root > /dev/null
-
-  if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-    MYSQL_ROOT_PASSWORD=111111
-    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-  fi
-
-  MYSQL_DATABASE="${1}"
-  MYSQL_USER=${MYSQL_USER:-""}
-  MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
-
-  tfile=`mktemp`
-  if [ ! -f "$tfile" ]; then
-      return 1
-  fi
-
-  cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-EOF
-
-  if [ "$MYSQL_DATABASE" != "" ]; then
-    echo "[i] Creating database: $MYSQL_DATABASE"
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-
-    if [ "$MYSQL_USER" != "" ]; then
-      echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-      echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-    fi
-  fi
-
-  /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
-  rm -f $tfile
+  /usr/sbin/mysqld --user=root --initialize-insecure --verbose=0
 fi
 
-exec /usr/bin/mysqld --user=root --console
+exec /usr/sbin/mysqld --user=root --console
